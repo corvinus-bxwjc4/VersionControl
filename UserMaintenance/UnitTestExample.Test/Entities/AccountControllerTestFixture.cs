e0f2e99244dcf2fa102a4910e7efd50947b7d715
Dániel Csergő <daniel.csergo@stud.uni-corvinus.hu>
2021-11-23 22:49:53 +0100

    Register függvény HappyPath

--- UserMaintenance/UnitTestExample.Test/Entities/AccountControllerTestFixture.cs
+++ UserMaintenance/UnitTestExample.Test/Entities/AccountControllerTestFixture.cs
@@ -26,5 +26,39 @@ namespace UnitTestExample.Test.Entities
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+        [Test,
+        TestCase("abcd1234", false),
+        TestCase("irf@uni-corvinus", false),
+        TestCase("irf.uni-corvinus.hu", false),
+        TestCase("irf@uni-corvinus.hu", true)]
+        public void TestValidatePassword(string email, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [
+        Test,
+        TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+        TestCase("irf@uni-corvinus.hu", "Abcd1234567"),]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.Register(email, password);
+
+            // Assert
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
     }
 }
